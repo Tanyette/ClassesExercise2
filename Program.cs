a40a37a7c539c742e71b36b97a59c63f261316de
Tanyette Wiggison <bwaffiliates@yahoo.com>
2022-02-22 04:34:31 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Cars();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var mazda = new Cars()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Cars("Chevy", "Impala", 2001);
+
+            var carList = new List<Cars>() { myCar,mazda,chevy, new Cars()};
+
+            foreach(var vehicle in carList) 
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
+
+
+            
+        }
+    }
+}
